22d8c433c410a8020ba1066501d5255b0ab0f5a6
jeongjunyeong <nejunyoung@gmail.com>
2024-08-01 09:09:43 +0900

    20240801 root

---
+++ src/base/root.java
@@ -0,0 +1,15 @@
+package base;
+
+public class root {
+    public long solution(long n) {
+        double a = 0;
+        long anwser = -1;
+        if((Math.sqrt(n))%1==0){
+            a = Math.sqrt(n);
+            anwser = (long) ((a+1)*(a+1));
+        } else {
+            return anwser;
+        }
+        return anwser;
+    }
+}
